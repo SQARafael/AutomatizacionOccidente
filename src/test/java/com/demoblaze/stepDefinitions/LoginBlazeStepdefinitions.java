9807d968296cb0fed4f804794233e2dfed28c31e
SQARafael <rafael.chica@sqasa.co>
2023-11-10 12:07:40 -0500

    nunner + stepDef

---
+++ src/test/java/com/demoblaze/stepDefinitions/LoginBlazeStepdefinitions.java
@@ -0,0 +1,27 @@
+package com.demoblaze.stepDefinitions;
+/*
+ * @(#) LoginBlazeStepdefinitions.java 11.0
+ * Copyright 2023 SQA. Todos los derechos reservados.
+ */
+
+import io.cucumber.java.es.*;
+
+/**
+ * @autor Rafael Chica
+ * @Fecha: --o--
+ */
+public class LoginBlazeStepdefinitions {
+
+    @Dado("que el usuario abre l navegador e ingrese a login")
+    public void queElUsuarioAbreLNavegadorEIngreseALogin() {
+
+    }
+    @Cuando("el usuario ingrese las credenciales")
+    public void elUsuarioIngreseLasCredenciales() {
+
+    }
+    @Entonces("el usuario podrá ver la bienvenida del perfil")
+    public void elUsuarioPodráVerLaBienvenidaDelPerfil() {
+
+    }
+}
