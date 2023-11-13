286466764bb1724607f4559adc1c2ad3195008b2
panda-11128a <149093934+panda-11128a@users.noreply.github.com>
2023-11-12 19:41:41 -0500

    Spotify Login

--- src/test/java/com/spotify/open/stepDefinitions/loginSpotifySetepdefinitions.java
+++ src/test/java/com/spotify/open/stepDefinitions/loginSpotifySetepdefinitions.java
@@ -3,16 +3,24 @@ package com.spotify.open.stepDefinitions;
 import io.cucumber.java.es.*;
 
 public class loginSpotifySetepdefinitions {
-    @Dado("que el usuario abr el navegador e ingrese a login")
+    @Dado("que el usuario abre el navegador e ingrese a login")
     public void queElUsuarioAbreLNavegadorEIngreseALogin() {
 
     }
-    @Cuando("el usuario ingrese las credenciales")
-    public void elUsuarioIngreseLasCredenciales() {
+    @Cuando("el usuario le de en el boton Iniciar Sesion")
+    public void elUsuarioLeDeEnElBotonIniciarSesion() {
 
     }
-    @Entonces("el usuario podrá ver la bienvenida del perfil")
-    public void elUsuarioPodráVerLaBienvenidaDelPerfil() {
+    @Cuando("ingrese su usuario")
+    public void ingreseSuUsuario() {
+
+    }
+    @Cuando("ingrese su contraseña")
+    public void ingreseSuContraseña() {
+
+    }
+    @Entonces("el usuario podrá ver su perfil de Spotify")
+    public void elUsuarioPodráVerSuPerfilDeSpotify() {
 
     }
 }
--- src/test/resources/feautres/spotify/LoginSpotify.feature
+++ src/test/resources/feautres/spotify/LoginSpotify.feature
@@ -2,6 +2,9 @@
 Característica: Iniciar sesión en Spotify
 
   Escenario: Login exitoso en demo blaze
-    Dado que el usuario abre l navegador e ingrese a login
-    Cuando el usuario ingrese las credenciales
-    Entonces el usuario podrá ver la bienvenida del perfil
+    Dado que el usuario abre el navegador e ingrese a login
+    Cuando el usuario le de en el boton Iniciar Sesion
+    Y ingrese su usuario
+    Y ingrese su contraseña
+    Y el usuario le de en el boton Iniciar Sesion
+    Entonces el usuario podrá ver su perfil de Spotify
