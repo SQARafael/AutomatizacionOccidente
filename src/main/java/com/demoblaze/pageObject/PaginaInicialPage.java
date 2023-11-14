08fa596cb06113b2bd68411d5a560830206784cc
SQARafael <rafael.chica@sqasa.co>
2023-11-14 12:00:47 -0500

    POM arquitecture + code

---
+++ src/main/java/com/demoblaze/pageObject/PaginaInicialPage.java
@@ -0,0 +1,20 @@
+package com.demoblaze.pageObject;
+/*
+ * @(#) PaginaInicial.java 11.0
+ * Copyright 2023 SQA. Todos los derechos reservados.
+ */
+
+import net.serenitybdd.core.pages.PageObject;
+import org.openqa.selenium.By;
+
+/**
+ * @autor Rafael Chica
+ * @Fecha: --o--
+ */
+public class PaginaInicialPage extends PageObject {
+    private final By lnkLogin= By.xpath("//a[@id='login2']");
+
+    public By getLnkLogin() {
+        return lnkLogin;
+    }
+}
---
+++ src/main/java/com/demoblaze/steps/PaginaInicialSteps.java
@@ -0,0 +1,32 @@
+package com.demoblaze.steps;
+/*
+ * @(#) PaginaInicialSteps.java 11.0
+ * Copyright 2023 SQA. Todos los derechos reservados.
+ */
+
+import com.demoblaze.pageObject.PaginaInicialPage;
+import net.thucydides.core.annotations.Step;
+import org.fluentlenium.core.annotation.Page;
+
+/**
+ * @autor Rafael Chica
+ * @Fecha: --o--
+ */
+public class PaginaInicialSteps {
+
+    @Page
+    PaginaInicialPage paginaInicialPage;
+    /*PaginaInicialPage paginaInicialPage= new PaginaInicialPage();*/
+
+    @Step("Abrir el navegador")
+    public void abrirNavegador(){
+        paginaInicialPage.openUrl("https://www.demoblaze.com/");
+    }
+
+    @Step("Clic btn login")
+    public void clicLogin(){
+        paginaInicialPage.getDriver().findElement(paginaInicialPage.getLnkLogin())
+                .click();
+    }
+
+}
